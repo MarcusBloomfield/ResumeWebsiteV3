38006d4ec4c674550540b0de315725e77b06fe6e
Marcus Bloomfield <marcusalanbloomfield@gmail.com>
2023-08-19 02:16:45 +0800

    Edits

--- src/Components/AllCards.js
+++ src/Components/AllCards.js
@@ -1,23 +1,31 @@
-import Card from '../Components/Card.js';
+import Card from '../Components/Card.js';import '../Css/Animate.css'
+import '../Css/Common.css'
+import '../Css/Home.css'
+import '../Css/ProjectGrid.css'
+import '../Css/ProjectTile.css'
+import '../Css/Footer.css'
+import '../Css/Header.css'
+import '../Css/NavBar.css'
+import '../Css/AboutMe.css'
+import '../Css/Philosophy.css'
+import '../Css/Summary.css'
+import '../Css/ProjectPopUp.css'
+import '../Css/BackToTop.css'
+import '../Css/Link.css'
+import '../Css/Passion.css'
+import '../Css/HowILearned.css'
+import '../Css/Education.css'
+import '../Css/Experience.css'
+import '../Css/Proficiency.css'
 
 export default function AllCards() {
     return (<>
-        <Card className="card aboutMe " title=" About Me" details="
-            I am Marcus, programming is my passion and the creative problem-solving mindset that it requires is why I love it." />
-        <Card className="card philosophy" title="Philosophy" details="To program is to learn and adapt. To cease in these pursuits is to fail." />
-        <Card className="card howILearned" title="How I learned to program" details="
-I had a vision, to make the most complex advanced city-building game the world had ever seen.
-But alas, without any knowledge at all in programming or game design this was no easy journey. 
-I went from knowing nothing about programming, or art or unity or 3D modeling to proficient in all these areas. I spent countless hours researching, programming, learning and after hundreds of road blocks or failed projects I had finally made a half decent game. Although the work is never ending on my project and the complexity of it grows endlessly, I am always excited to continue work on my game." />
-        <Card className="card passion" title="Passion" details="Programming is my passion. I love it. I have spent countless hours creating complex systems and expanding my knowledge base. Without a doubt it would be a dream come true to have programming be my job." />
-        <Card className="card education" title="Education"
-            detailsTitle="2020 - 2021 Certificate IV in Programming, UpSkilled"
-            details="I have completed a certificate four in programming. In this course, I gained knowledge in agile development processes, application documentation standards, .Net, C#, SQL, and Xamarin."
-            details2Title={"2018 - Current Self-Taught"}
-            details2="I have been on a rigorous self-education journey ever since I began to mess with programming back in 2018. I love programming and will always love learning new languages as well as building on what I already know." />
-        <Card className="card experience" title="Experience"
-            detailsTitle="2019 - 2021 Unity, C# Developer Self Employed"
-            details="In 2019 my first app launched on the Google Play Store. I have also been working on smaller side projects which include smaller games and react-based websites as well as my passion project. My passion project is a city-building strategy game that has an in-game intelligence system, which is intended to allow for a greater level of realism. Making games is a huge challenge but I welcome it." />
-        <Card className="card summary" title="Summary" details=" I am always learning, and constantly developing my skills, I love programming, developing systems, and creating applications. I am always working on another project in my spare time to both expand my knowledge base and for my own enjoyment." />
+        <Card className="card aboutMe " title="About Me" />
+        <Card className="card howILearned" title="HowILearned"  />
+        <Card className="card passion" title="Passion" />
+        <Card className="card education" title="Education" />
+        <Card className="card experience" title="Experience" />
+        <Card className="card proficiency" title="Proficiency" />
+        <Card className="card summary" title="Summary" />
     </>)
 }
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,19 +5,129 @@ export default function Card({ ...props }) {
         <div data-aos="fade-in" className={props.className}>
             <div>
                 <div className="cardLayout">
-                    <div className='cardDetails'>
-                        <div>
-                            <h2> {props.title} </h2 >
-                            <h4>{props.detailsTitle}</h4>
-                            <div> {props.details} </div>
-                        </div>
-                        <div>
-                            <h4>{props.details2Title}</h4>
-                            <div> {props.details2} </div>
-                        </div>
+                    <div className='cardText'>
+
+                       { Switch(props.title)}
+
                     </div>
                 </div>
             </div>
         </div >
     );
+}
+
+function Switch(name){
+
+    switch (name) {
+        case "About Me":
+           return AboutMe()
+    
+        case "HowILearned":
+            return HowILearned()
+        
+        case "Passion":
+            return Passion()
+
+        case "Education":
+            return Education()
+                
+        case "Experience":
+            return Experience()
+            
+        case "Proficiency":
+            return Proficiency()
+        
+        case "Summary":
+            return Summary()
+
+        default:
+            return <h1> ITS BROKEN </h1>
+    }
+}
+
+function AboutMe()
+{
+    return(
+        <div>
+            <h2> About Me </h2 >
+            <div>  I am Marcus, programming is my passion and the creative problem-solving mindset that it requires is why I love it. </div>
+        </div>
+    )
+}
+function HowILearned()
+{
+    return(
+        <div>
+            <h2> How I Learnt Programming </h2 >
+            <div> I had a vision, to make the most complex advanced city-building game the world had ever seen. </div>
+            <div> I went from knowing nothing about programming, or art or unity or 3D modeling to proficient in all these areas. </div>
+            <div> I have spent countless hours researching, programming and learning.</div>
+            <div>Although the work is never ending on my project and the complexity of it grows endlessly. </div>
+            <div>I am always excited to continue work on my game.</div>
+        </div>
+    )
+}
+function Passion()
+{
+    return(
+        <div>
+            <h2> Passion </h2 >
+            <div>Programming is my passion.</div>
+            <div>I have spent countless hours creating complex systems and expanding my knowledge base. </div>
+            <div>Without a doubt it would be a dream come true to have programming be my job.</div>
+        </div>
+    )
+}
+function Education()
+{
+    return(
+        <div>
+            <h2> Education </h2 >
+            <div>
+                <h4>2020 - 2021 Certificate IV in Programming, UpSkilled</h4>
+                <div> I have completed a certificate four in programming.  </div>
+                <div>I gained knowledge in agile development processes, application documentation standards, .Net, C#, SQL, and Xamarin.</div>
+             </div>
+            <div>
+                <h4>2018 - Current Self-Taught</h4>
+                <div> I have been on a rigorous self-education journey ever since I began to mess with programming back in 2018. </div>
+                <div>I love programming and will always love learning new languages as well as building on what I already know. </div>
+            </div>
+        </div>
+    )
+}
+function Experience()
+{
+    return(
+        <div>
+            <h2> Experience </h2 >
+            <div>
+                <h4>2019 - 2021 Unity, C# Developer Self Employed</h4>
+                <div> In 2019 my first app launched on the Google Play Store. </div>
+                <div>I am always working on side projects, React website, Unity games and other projects in my spare time.</div>
+             </div>
+        </div>
+    )
+}
+function Proficiency()
+{
+    return(
+        <div>
+            <h2> Proficiency </h2 >
+            <div> C# is my main programming language, I prefer it over all the others and am well versed in it. </div>
+            <div>HLSL is probably one of the most interesting languages I know as it is used to create shaders. </div>
+            <div>I am also capable in SQL, HTML, Javascript and python. </div>
+        </div>
+    )
+}
+function Summary()
+{
+    return(
+        <div>
+            <h2> Summary </h2 >
+            <div> I am always learning, and constantly developing my skills. </div>
+            <div>I love programming, developing systems, and creating applications. </div>
+            <div>I am always working on another project in my spare time to both expand my knowledge base and for my own enjoyment.</div>
+        </div>
+    )
 }
